4e0c3d7ee18cb8c9ce8f2cb3d2a3058d99985ca0
pwrstudio <pwrpntr@gmail.com>
2023-11-24 23:06:51 +0100

    feat: expanded item

--- schemas/Item.ts
+++ schemas/Item.ts
@@ -7,6 +7,9 @@ export default {
     title: 'Item',
     icon: MdArticle,
     type: 'document',
+    initialValue: {
+        type: 'article'
+    },
     fields: [
         {
             name: 'title',
@@ -15,9 +18,55 @@ export default {
             validation: (Rule: any) => Rule.required(),
         },
         {
+            name: 'date',
+            title: 'Date',
+            type: 'date',
+        },
+        {
+            name: 'location',
+            title: 'Location',
+            type: 'string',
+        },
+        {
+            name: 'tags',
+            title: 'Tags',
+            type: 'array',
+            of: [{ type: 'string' }],
+            options: {
+                layout: 'tags',
+            },
+        },
+        {
+            name: 'type',
+            title: 'Type',
+            type: 'string',
+            options: {
+                list: [
+                    { title: 'Article', value: 'article' },
+                    { title: 'Link', value: 'link' },
+                    { title: 'File', value: 'file' },
+                ],
+                layout: 'radio',
+            },
+            validation: (Rule: any) => Rule.required(),
+        },
+        {
             name: 'content',
             title: 'Content',
             type: 'contentEditor',
+            hidden: ({ document }) => document.type !== 'article',
+        },
+        {
+            name: 'link',
+            title: 'Link',
+            type: 'url',
+            hidden: ({ document }) => document.type !== 'link',
+        },
+        {
+            name: 'file',
+            title: 'File',
+            type: 'file',
+            hidden: ({ document }) => document.type !== 'file',
         },
         {
             name: 'slug',
